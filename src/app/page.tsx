a34f8eaeee9de05e9fc6ee0c846f6225976c7166
David <ejeredavid2001@gmail.com>
2024-10-16 02:32:37 +0100

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import Navbar from '@/app/components/Navbar'
-import Hero from '@/app/components/hero'
-import About from '@/app/components/About'
-import Book from "@/app/components/booksession"
-import Footer from '@/app/components/footer'
+import dynamic from 'next/dynamic';
+
+// Dynamically import components with SSR disabled
+const Navbar = dynamic(() => import('@/app/components/Navbar'), { ssr: false });
+const Hero = dynamic(() => import('@/app/components/hero'), { ssr: false });
+const About = dynamic(() => import('@/app/components/About'), { ssr: false });
+const Book = dynamic(() => import('@/app/components/booksession'), { ssr: false });
+const Footer = dynamic(() => import('@/app/components/footer'), { ssr: false });
 
 export default function Home() {
   return (
-<div>
-  <Navbar/>
-  <Hero/>
-  <About/>
-  <Book/>
-  <Footer/>
-
-</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Book />
+      <Footer />
+    </div>
   );
 }
